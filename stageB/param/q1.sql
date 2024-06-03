5e2797e10a463aa25c2726e0d3ccf94666bd228f
lazyturtel <110930193+lazyturtel@users.noreply.github.com>
2024-06-03 18:45:56 +0300

    asdfghjkl

--- stageB/param/q1.sql
+++ stageB/param/q1.sql
@@ -1,4 +1,6 @@
 -- Query 1: Select the names of drivers hired after a specified date
+DECLARE 
+    hire_date DATE;
 SELECT d.FullName, d.HireDate
 FROM Driver d
 WHERE d.HireDate > TO_DATE(:hire_date, 'YYYY-MM-DD')
