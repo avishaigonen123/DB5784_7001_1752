de571a5c7287756e36f769eb9198a2ea6985e882
lazyturtel <110930193+lazyturtel@users.noreply.github.com>
2024-06-03 18:18:54 +0300

    finished q1

--- stageB/update/queryUpdate1.sql
+++ stageB/update/queryUpdate1.sql
@@ -1,7 +1,14 @@
 -- UPDATE:
 
--- Query 1: Update the working zone of all taxis operating between 8 AM and 4 PM to 'Zone B'
+-- Query 1: Update the working zone of all taxis operating between 7 AM and 8 AM to 'Nahariya'
+
+SELECT *
+FROM Taxi;
+
 UPDATE Taxi
-SET WorkingZone = 'Zone B'
-WHERE TO_TIMESTAMP(StartTime, 'HH24:MI:SS') >= TO_TIMESTAMP('08:00:00', 'HH24:MI:SS')
-  AND TO_TIMESTAMP(FinishTime, 'HH24:MI:SS') <= TO_TIMESTAMP('16:00:00', 'HH24:MI:SS');
+SET WorkingZone = 'Nahariya'
+WHERE TO_TIMESTAMP(StartTime, 'HH24:MI:SS') >= TO_TIMESTAMP('07:00:00', 'HH24:MI:SS')
+  AND TO_TIMESTAMP(FinishTime, 'HH24:MI:SS') <= TO_TIMESTAMP('08:00:00', 'HH24:MI:SS');
+
+SELECT *
+FROM Taxi;
