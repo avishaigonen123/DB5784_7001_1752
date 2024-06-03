9a50e43b3762215ee39b678c6dfc7aa4130a4ae8
avishaigonen123 <avishaigonen@gmail.com>
2024-06-03 18:14:38 +0300

    changed some update files

--- stageB/update/queryUpdate1.sql
+++ stageB/update/queryUpdate1.sql
@@ -1,12 +1,7 @@
 -- UPDATE:
-/*
+
 -- Query 1: Update the working zone of all taxis operating between 8 AM and 4 PM to 'Zone B'
 UPDATE Taxi
 SET WorkingZone = 'Zone B'
 WHERE TO_TIMESTAMP(StartTime, 'HH24:MI:SS') >= TO_TIMESTAMP('08:00:00', 'HH24:MI:SS')
   AND TO_TIMESTAMP(FinishTime, 'HH24:MI:SS') <= TO_TIMESTAMP('16:00:00', 'HH24:MI:SS');
-
--- Query 2: Update the capacity of buses purchased in the year 2020 to 60
-UPDATE Bus
-SET Capacity = 60
-WHERE EXTRACT(YEAR FROM PurchaseDate) = 2020;
